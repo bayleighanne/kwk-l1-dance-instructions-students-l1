b4712c722c173c04de00eeeea52f1a56009bd633
bayleighanne <bayhaf718@gmail.com>
2018-06-04 23:02:10 +0000

    automatically backed up by learn

--- about_me.rb
+++ about_me.rb
@@ -1,12 +1,3 @@
-def about_me 
-puts "My name is Bayleigh"
-puts "I am from Phoenix,AZ"
-puts "I am 15 years old"
-puts "My favorite food is chocolate"
-30.times do 
-about_me
-end
-
 puts "how are you feeling?"
 answer = gets.chomp
 puts "so you're feeling #{answer}?"
