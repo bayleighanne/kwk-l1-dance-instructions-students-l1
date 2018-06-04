5354578dcc20f0489f1abc212f10b2a6c728bb44
bayleighanne <bayhaf718@gmail.com>
2018-06-04 23:08:33 +0000

    automatically backed up by learn

--- about_me.rb
+++ about_me.rb
@@ -1,6 +1,7 @@
-puts "how are you feeling?"
-answer = gets.chomp
-puts "so you're feeling #{answer}?"
+#puts "how are you feeling?"
+#answer = gets.chomp
+#puts "so you're feeling #{answer}?"
+
 puts "what's your age"
 answer = gets.chomp 
 puts "so your age is #{answer}?"
